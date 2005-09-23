05e8d8f7189966a3c1d4e0028d6c886591d93db8
baijum81 <baijum81@8f0c695e-9100-0410-9f95-92c9322a4aec>
2005-09-23 13:41:54 +0000

    * ftests/common.py: * ftests/test_comment.py:
    functional tests for comment details.

--- trunk/zissue/ftests/common.py
+++ trunk/zissue/ftests/common.py
@@ -1,22 +1,70 @@
 import unittest
 from zope.app.testing.functional import BrowserTestCase
 
-class CommentTestCase(BrowserTestCase):
 
+class CommentTestCase(BrowserTestCase):
+    #zt - ZissueTracker
+    #mp - MainProduct
+    #sp - SubProduct
+    #cmp - Component
+    #zi - Zissue
+    #cmm - Comment
     def testAddZissueTracker(self):
-        pass
+        response = self.publish(
+            '/+/AddZissueTracker.html=zt',
+            basic='mgr:mgrpw',
+            form={'field.name': u'MyZissueTracker',
+                  'field.description': u'My Zissue Tracker',
+                  'UPDATE_SUBMIT': 'Add'})
+        self.assertEqual(response.getStatus(), 302)
+        self.assertEqual(response.getHeader('Location'),
+                         'http://localhost/@@contents.html')
 
     def testAddMainProduct(self):
-        pass
+        self.testAddZissueTracker()
+        response = self.publish(
+            '/zt/+/AddMainProduct.html=mp',
+            basic='mgr:mgrpw',
+            form={'field.name': u'MyMainProduct',
+                  'field.description': u'My Main Product',
+                  'UPDATE_SUBMIT': 'Add'})
+        self.assertEqual(response.getStatus(), 302)
+        self.assertEqual(response.getHeader('Location'),
+                         'http://localhost/zt/@@contents.html')
 
     def testAddComponent(self):
-        pass
-    
+        self.testAddMainProduct()
+        response = self.publish(
+            '/zt/mp/+/AddComponent.html=cmp',
+            basic='mgr:mgrpw',
+            form={'field.name': u'MyComponent',
+                  'field.description': u'My Component',
+                  'UPDATE_SUBMIT': 'Add'})
+        self.assertEqual(response.getStatus(), 302)
+        self.assertEqual(response.getHeader('Location'),
+                         'http://localhost/zt/mp/@@contents.html')
+
     def testAddZissue(self):
-        pass
+        self.testAddComponent()
+        response = self.publish(
+            '/zt/mp/cmp/+/AddZissue.html=zi',
+            basic='mgr:mgrpw',
+            form={'field.summary': u'MyZissue',
+                  'UPDATE_SUBMIT': 'Add'})
+        self.assertEqual(response.getStatus(), 302)
+        self.assertEqual(response.getHeader('Location'),
+                         'http://localhost/zt/mp/cmp/@@contents.html')
 
     def testAddComment(self):
+        self.testAddZissue()
+        response = self.publish(
+            '/zt/mp/cmp/zi/+/AddComment.html=cmm',
+            basic='mgr:mgrpw',
+            form={'field.note': u'My Comment',
+                  'UPDATE_SUBMIT': 'Add'})
+        self.assertEqual(response.getStatus(), 302)
+        self.assertEqual(response.getHeader('Location'),
+                         'http://localhost/zt/mp/cmp/zi/@@contents.html')
+        
         pass
 
-    def testCommentDetails(self):
-        pass
--- trunk/zissue/ftests/test_comment.py
+++ trunk/zissue/ftests/test_comment.py
@@ -1,8 +1,20 @@
 import unittest
 import common
 
+
 class CommentTest(common.CommentTestCase):
-    pass
+
+    def testCommentDetails(self):
+        self.testAddComment()
+        response = self.publish(
+            '/zt/mp/cmp/zi/cmm/@@comments.html',
+            basic='mgr:mgrpw')
+        body = response.getBody()
+        self.checkForBrokenLinks(body, '/zt/mp/cmp/zi/cmm/@@comments.html',
+                                 basic='mgr:mgrpw')
+        self.assert_(body.find('My Comment') > 0)
+        pass
+
 
 def test_suite():
     return unittest.TestSuite((
