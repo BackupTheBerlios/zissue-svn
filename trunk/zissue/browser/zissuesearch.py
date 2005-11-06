676b180b971691e6a23163fa519574a46a325523
baijum81 <baijum81@8f0c695e-9100-0410-9f95-92c9322a4aec>
2005-11-06 04:50:47 +0000

    * zissue/browser/zissuesearch.py: * zissue/browser/zissuesearch.pt:
    Summary searching done.

--- trunk/zissue/browser/zissuesearch.py
+++ trunk/zissue/browser/zissuesearch.py
@@ -1,4 +1,5 @@
 from zissue.interfaces import IMainProduct
+from zissue.interfaces import ISubProduct
 from zissue.interfaces import IComponent
 from zissue.interfaces import IZissue
 
@@ -14,15 +15,21 @@ class ZissueSearch:
                         for name2, child2 in child1.items():
                             if IZissue.providedBy(child2):
                                 if child2.summary.find(searchText) >= 0:
-                                    results.append(name2)
+                                    info = {}
+                                    info['name'] = name2
+                                    info['url'] = name+"/"+name1+"/"+name2
+                                    results.append(info)
 
                     elif ISubProduct.providedBy(child1):
-                        for name3, child3 in child.items():
+                        for name3, child3 in child1.items():
                             if IComponent.providedBy(child3):
                                 for name4, child4 in child3.items():
                                     if IZissue.providedBy(child4):
                                         if child4.summary.find(searchText) >= 0:
-                                            results.append(name4)
+                                            info = {}
+                                            info['name'] = name4
+                                            info['url'] = name+"/"+name1+"/"+name3+"/"+name4
+                                            results.append(info)
 
         return {'results': results,
                 'total': len(results)}
