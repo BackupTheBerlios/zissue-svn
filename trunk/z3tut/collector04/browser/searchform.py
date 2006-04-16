c6be0238e51e45cedf328d05a4494a7f58146693
baijum81 <baijum81@8f0c695e-9100-0410-9f95-92c9322a4aec>
2006-04-16 09:54:17 +0000

    * collector04/browser/searchform.py: catalog tested.

--- trunk/z3tut/collector04/browser/searchform.py
+++ trunk/z3tut/collector04/browser/searchform.py
@@ -1,8 +1,19 @@
 
+from zope.app.catalog.interfaces import ICatalog
+from zope.app import zapi
+
+CATALOG = 'cat1'
+
 class CollectorSearch:
-    
+
     def searchSimpleText(self):
+        catalog = zapi.getUtility(ICatalog, CATALOG)
         searchText = self.request.get('searchText', '')
+
+        result = catalog.searchResults(TextIndex=searchText)
+        gen = iter(result)
+        print gen.next()
+
         results = [{'name': 'test',
                     'url': 'turl'}]
         
